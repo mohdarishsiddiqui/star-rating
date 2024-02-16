372b828ffba867b28e6d93a38f6f657d41a27f7a
mohdarishsiddiqui <arishsiddiqui5@gmail.com>
2024-02-16 18:59:13 +0530

    first commit

---
+++ star-rating/src/App.jsx
@@ -0,0 +1,13 @@
+import StarRating from '.'
+
+function App() {
+
+
+  return (
+  <>
+  <StarRating/>
+    </>
+  )
+}
+
+export default App
---
+++ star-rating/src/index.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import "./style.css";
+
+export default function StarRating({ noOfStars = 7 }) {
+  const [rating, setRating] = useState(0);
+  const [hover, sethover] = useState(0);
+
+  function handleClick(getCurrentIndex) {
+    setRating(getCurrentIndex);
+  }
+  function handleMouseEnter(getCurrentIndex) {
+    sethover(getCurrentIndex);
+  }
+  function handleMouseLeave() {
+    sethover(rating);
+  }
+
+  return (
+    <div className="star-rating">
+      {[...Array(noOfStars)].map((_, index) => {
+        index += 1;
+        return (
+          <FaStar
+            key={index}
+            className={index <= (hover || rating) ? "active" : "inactive"}
+            onClick={() => handleClick(index)}
+            onMouseMove={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave()}
+            size={40}
+          />
+        );
+      })}
+    </div>
+  );
+}
